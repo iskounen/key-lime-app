57b21f25e65795ea6208ce042d888eae4539dffe
Adam Iskounen <adam.iskounen@artsymail.com>
2022-01-29 12:59:10 -0500

    Populated a message from the key-lime-api.

--- key-lime-app/ContentView.swift
+++ key-lime-app/ContentView.swift
@@ -1,9 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "Loading..."
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(message)
             .padding()
+            .onAppear() {
+                let session = URLSession.shared
+                let url = URL(string: "https://key-lime-api.herokuapp.com/hello_world")
+                let task = session.dataTask(with: url!, completionHandler: { data, response, error in
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    
+                    message = String(data: data!, encoding: .utf8)!
+                })
+                
+                task.resume()
+            }
     }
 }
 
